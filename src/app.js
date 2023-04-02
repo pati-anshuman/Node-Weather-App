0d9fe49e8e3e9a7e8eb6c315290de25717550293
AnshumanPati1999 <anshupati2012@gmail.com>
2023-04-02 17:22:17 +0530

    Comments Deleted

--- src/app.js
+++ src/app.js
@@ -51,14 +51,7 @@ app.get('/weather',(req,res) => {
             error: 'you must provide an address'
         })
     }
-
-    // res.send({
-    //     forecast: 'It is sunny',
-    //     location: 'Bankura',
-    //     address: req.query.address
-    // })
-
-
+    
     geocode(req.query.address, (error,{latitude,longitude,location,country} = {}) => {
     if(error) {
         return res.send({ error })
